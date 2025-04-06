01cf14c70be4aa793a17859fce8114d5feba3276
jey <jeyshreemen.info@gmail.com>
2025-04-06 22:35:09 +0530

    first commit

---
+++ app/onboarding.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const onboarding = () => {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text>onboarding</Text>
+    </View>
+  )
+}
+
+export default onboarding
+
+const styles = StyleSheet.create({})
