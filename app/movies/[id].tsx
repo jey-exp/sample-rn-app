a639cfbf5103ed8a3e0e64c3b296fc584c96488d
jey <jeyshreemen.info@gmail.com>
2025-04-08 01:17:39 +0530

    feature: added proper home page

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams } from 'expo-router'
 
 const MovieDetails = () => {
+  const {id} = useLocalSearchParams();
   return (
     <View>
-      <Text>Movie</Text>
+      <Text>Movie name : {id}</Text>
     </View>
   )
 }
